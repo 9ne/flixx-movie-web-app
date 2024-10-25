3321fe451699e4ebacbb6dbc68c04787dcebed25
Martin Naumoski <jajonaumoski@gmail.com>
2024-10-25 19:15:37 +0200

    search done, displaying show details done, using swiper library doen

--- js/script.js
+++ js/script.js
@@ -1,17 +1,45 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1
+  },
+  api: {
+    apiKey: '75379d6cabfbe95d5c7840dd6ebd49d7',
+    apiUrl: 'https://api.themoviedb.org/3/'
+  }
 }
 
 
 // Fetch data from TMDB API
 const fetchApiData = async (endpoint) => {
-  const API_KEY = '75379d6cabfbe95d5c7840dd6ebd49d7';
-  const API_URL = 'https://api.themoviedb.org/3/';
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
   showSpinner();
   
   const res = await fetch(`
     ${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US
+  `);
+
+  const data = await res.json();
+
+  hideSpinner();
+
+  return data;
+};
+
+// Make request to seach
+const searchApiData = async () => {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+  
+  const res = await fetch(`
+    ${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}
   `);
 
   const data = await res.json();
@@ -181,6 +209,75 @@ const displayMovieDetails = async () => {
     document.querySelector('#movie-details').appendChild(div);
 };
 
+// Display show details
+const displayShowDetails = async () => {
+  const showId = window.location.search.split('=')[1];
+  const show = await fetchApiData(`tv/${showId}`);
+  // console.log(movie);
+  // Overlayh for background image
+  displayBackgroundImage('tv', show.backdrop_path);
+  const div = document.createElement('div');
+  div.innerHTML = 
+    `
+      <div class="details-top">
+          <div>
+          ${
+            show.poster_path ?
+            `
+            <img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+            class="card-img-top"
+            alt="${show.name}"
+            />
+            `
+            : 
+            `
+            <img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${show.name}"
+            />
+            `
+          }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Aired: ${show.last_air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map((genre) => 
+                `<li>${genre.name}</li>`
+              ).join('')}
+            </ul>
+            <a href="${show.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number of episodes:</span> ${show.number_of_episodes}</li>
+            <li><span class="text-secondary">Last episode to air:</span> ${show.last_episode_to_air.name}</li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+              ${show.production_companies.map((company) =>
+                `<span>${company.name}</span>`
+              ).join(', ')}
+          </div>
+        </div>
+    `
+    document.querySelector('#show-details').appendChild(div);
+};
+
+
 // Display backdrop on detail pages
 const displayBackgroundImage = (type, backgroundPath) => {
   const overlayDiv = document.createElement('div');
@@ -203,6 +300,71 @@ const displayBackgroundImage = (type, backgroundPath) => {
   }
 };
 
+// Search Movies/Shows
+const search = async () => {
+  const queryString = window.location.search;
+  // console.log(queryString);
+  const urlParams = new URLSearchParams(queryString);
+  // console.log(urlParams.get('type'));
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+
+  if (global.search.term !== '' && global.search.term !== null) {
+    const results = await searchApiData();
+    console.log(results);
+  } else {
+    showAlert('Please type a key word for seaching');
+  }
+};
+
+// Display Slider Movies
+const displaySlider = async () => {
+  const { results } = await fetchApiData('movie/now_playing');
+  console.log(results);
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = 
+      `
+      <div class="swiper-slide">
+        <a href="movie-details.html?id=${movie.id}">
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+        </a>
+        <h4 class="swiper-rating">
+          <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(1)} / 10
+        </h4>
+      </div>
+      `
+    document.querySelector('.swiper-wrapper').appendChild(div);
+    
+    initSwiper();
+  })
+};
+
+const initSwiper = () => {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2
+      },
+      700: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      }
+    }
+  });
+};
+
 
 // Highlight active link
 const highlightActiveLink = () => {
@@ -212,6 +374,15 @@ const highlightActiveLink = () => {
       link.classList.add('active');
     }
   });
+};
+
+// Show Alert
+const showAlert = (message, className) => {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+  setTimeout(() => alertEl.remove(), 3000);
 };
 
 const addCommasToNumber = (number) => {
@@ -224,19 +395,20 @@ const addCommasToNumber = (number) => {
 const init = () => {
   switch(global.currentPage) {
     case '/flixx-web-movie-app/index.html':
+      displaySlider();
       displayPopularMovies();
       break;
     case '/flixx-web-movie-app/shows.html':
       displayPopularTVShows();
       break;
     case '/flixx-web-movie-app/tv-details.html':
-      console.log('Tv Details');
+      displayShowDetails();
       break;
     case '/flixx-web-movie-app/movie-details.html':
       displayMovieDetails();
       break;
     case '/flixx-web-movie-app/search.html':
-      console.log('Search');
+      search();
       break;
   }
 
